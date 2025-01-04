e00055898a3b872733bb152c93dda2ae025eb416
Mohammad Shujaullah <mdshujaullah789@gmail.com>
2025-01-05 02:03:23 +0530

    Time: 0 ms (100%), Space: 46.3 MB (5.47%) - LeetHub

---
+++ 0792-binary-search/0792-binary-search.java
@@ -0,0 +1,25 @@
+// we can solve this problem by iterative as well as recursive
+// iterative is very comman ,so we sovle by reccussion
+
+class Solution {
+    public int solve(int[] nums, int l, int r, int target) {
+        if (l > r) {
+            return -1;
+        }
+        int mid = l + (r - l) / 2;
+        if (nums[mid] == target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            return solve(nums, mid + 1, r, target);
+        } else {
+            return solve(nums, l, mid - 1, target);
+        }
+
+    }
+
+    public int search(int[] nums, int target) {
+        int n=nums.length;
+        return solve(nums,0,n-1,target);
+
+    }
+}
