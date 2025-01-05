6d5d9ea98349f73601b8286309aa5de5e435e78f
Mohammad Shujaullah <mdshujaullah789@gmail.com>
2025-01-06 01:21:27 +0530

    Time: 0 ms (100%), Space: 42 MB (59.37%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,45 @@
+// search the target in two partitioned array, 
+//patition by pivoted element or smallest 
+
+class Solution {
+    private int searchPivote(int[] nums, int n) {
+        int l = 0;
+        int r = n - 1;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] > nums[r]) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        return r;
+    }
+
+    private int binarySearch(int[] nums, int l, int r, int target) {
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                l = mid + 1;
+
+            } else {
+                r = mid - 1;
+            }
+        }
+        return -1;
+
+    }
+
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int pivoted_el = searchPivote(nums, n);
+        int idx = binarySearch(nums, 0, pivoted_el - 1, target);
+        if (idx != -1) {
+            return idx;
+        }else{
+            return binarySearch(nums, pivoted_el,n-1, target);
+        }
+    }
+}
