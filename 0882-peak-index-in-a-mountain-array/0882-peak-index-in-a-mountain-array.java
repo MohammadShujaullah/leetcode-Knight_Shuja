fa0103b78e12b40000b6ece611aed23aea42f053
Mohammad Shujaullah <mdshujaullah789@gmail.com>
2025-01-08 02:35:47 +0530

    Update 0882-peak-index-in-a-mountain-array.java
    added explaination of some line of code

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.java
@@ -5,9 +5,9 @@ class Solution {
         while (l < r) {
             int mid = l + (r - l) / 2;
             if (arr[mid] < arr[mid + 1]) {
-                l = mid + 1;
+                l = mid + 1;     // the mountain must be greater ,so l can start  with mid+1
             } else {
-                r = mid;
+                r = mid;          // the moutain must be greater so ,we cannot neglect mid element ,so r stat with mid
             }
         }
         return l;
