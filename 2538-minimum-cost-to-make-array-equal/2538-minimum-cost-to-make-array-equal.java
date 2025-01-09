6d1e5e481a125968c502bd15674a0897923b764f
Mohammad Shujaullah <mdshujaullah789@gmail.com>
2025-01-10 02:29:38 +0530

    Time: 13 ms (63.89%), Space: 56.8 MB (38.19%) - LeetHub

--- 2538-minimum-cost-to-make-array-equal/2538-minimum-cost-to-make-array-equal.java
+++ 2538-minimum-cost-to-make-array-equal/2538-minimum-cost-to-make-array-equal.java
@@ -1,31 +1,33 @@
-class Solution {
-    private long findCost(int[] nums, int[] cost, int target) {
-        long result = 0;
+ import java.util.*;
 
-        for (int j = 0; j < nums.length; j++) {
-            result += (long)(Math.abs(target - nums[j])) * cost[j];
+class Solution {
+    public long getCost(int[] nums, int[] cost, int target) {
+        long result = 0;
+        for (int i = 0; i < nums.length; ++i) {
+            result += (long) Math.abs(nums[i] - target) * cost[i];
         }
         return result;
     }
 
     public long minCost(int[] nums, int[] cost) {
         long answer = Long.MAX_VALUE;
-        int l = Arrays.stream(nums).min().getAsInt();
-        int r = Arrays.stream(nums).max().getAsInt();
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
-            long cost1 = findCost(nums, cost, mid);
-            long cost2 = findCost(nums, cost, mid + 1);
+
+        int left = Arrays.stream(nums).min().getAsInt();  // Minimum value in nums
+        int right = Arrays.stream(nums).max().getAsInt(); // Maximum value in nums
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            long cost1 = getCost(nums, cost, mid);
+            long cost2 = getCost(nums, cost, mid + 1);
+
             answer = Math.min(cost1, cost2);
             if (cost1 > cost2) {
-                l = mid + 1;
-
+                left = mid + 1;
             } else {
-                r = mid - 1;
+                right = mid - 1;
             }
-
         }
         return answer == Long.MAX_VALUE ? 0 : answer;
-
     }
 }
