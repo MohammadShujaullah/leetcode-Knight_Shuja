d172a9ecf13b66e0a442acd27ff4242da235103c
Mohammad Shujaullah <mdshujaullah789@gmail.com>
2025-01-12 19:09:13 +0530

    Time: 56 ms (25.47%), Space: 49 MB (47.64%) - LeetHub

--- 2732-counter/2732-counter.js
+++ 2732-counter/2732-counter.js
@@ -3,9 +3,10 @@
  * @return {Function} counter
  */
 var createCounter = function(n) {
-
+     let count=n;
     return function() { 
-     return n++;
+        count++;
+     return count-1;
       
         
     };
