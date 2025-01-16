70f41b570e2468dec8548e087579964ee27d0936
Mohammad Shujaullah <mdshujaullah789@gmail.com>
2025-01-17 02:37:59 +0530

    Time: 0 ms (100%), Space: 42.1 MB (71.57%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,30 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int m=matrix.length;
+        int n=m;
+        for(int i=0;i<m;i++){
+            for(int j=i;j<m;j++){
+            // taking the transpose of the matrix
+            int temp=matrix[i][j];
+           matrix[i][j]= matrix[j][i];
+            matrix[j][i]=temp;
+
+            }
+        }
+
+        // now reversing the 1st column with last column
+        for(int i=0;i<m;i++){
+            int start=0;
+            int end=m-1;
+            while(start<end){
+               int comp=matrix[i][start];
+            matrix[i][start]=matrix[i][end];
+            matrix[i][end]=comp;
+
+            start++;
+            end--;
+            }
+           
+        }
+    }
+}
