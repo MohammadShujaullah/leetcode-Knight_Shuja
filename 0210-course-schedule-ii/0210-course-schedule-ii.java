0c0ba0c45ed13aaf7d71564e5579ab0d3c527442
Mohammad Shujaullah <mdshujaullah789@gmail.com>
2025-02-05 23:03:18 +0530

    Time: 8 ms (20.96%), Space: 46.3 MB (27.8%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,16 +1,40 @@
 class Solution {
+    boolean isCycle;
+
+    private void CycleDFS(Map<Integer, ArrayList<Integer>> mp, int u, boolean visited[], boolean inRecurssion[],
+            Stack<Integer> st) {
+        visited[u] = true;
+        inRecurssion[u] = true;
+
+        if (mp.get(u) != null) {
+            for (int v : mp.get(u)) {
+                if (!visited[v]) {
+                    CycleDFS(mp, v, visited, inRecurssion, st);
+
+                } else if (inRecurssion[v] == true) {
+                    isCycle = true;
+
+                    return;
+                }
+            }
+        }
+        inRecurssion[u]=false;
+        st.push(u);
+        return;
+
+    }
+
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         // this question directly wants you to find the ,totpological sorted array of
         // the course , and if not find the totpological array , then return empty array
+
         ArrayList<Integer> result = new ArrayList<>();
+
         if (prerequisites.length == 0) {
             for (int i = 0; i < numCourses; i++)
                 result.add(i);
             return result.stream().mapToInt(i -> i).toArray();
         }
-
-        // indegree
-        int indegree[] = new int[numCourses];
 
         Map<Integer, ArrayList<Integer>> mp = new HashMap<>();
 
@@ -24,46 +48,33 @@ class Solution {
             }
             mp.get(prerequise).add(course);
 
-            // calculate the indegree
-            indegree[course]++;
-
         }
+        boolean visited[] = new boolean[numCourses];
+        boolean inRecurssion[] = new boolean[numCourses];
+        isCycle = false;
 
-        int count = 0;
+        Stack<Integer> st = new Stack<>();
 
-        // intro queue
-        Queue<Integer> q = new LinkedList<>();
-
-        // populate the queue with indegree ==0
         for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
-                q.add(i);
-                count++;
-
-                result.add(i);
-
+            if (!visited[i]) {
+                CycleDFS(mp, i, visited, inRecurssion, st);
             }
+
+        }
+        if (isCycle == true) {
+            return new int[] {};
+
         }
 
-        // step 3 BFS
-        while (!q.isEmpty()) {
-            int u = q.remove();
-            if (mp.get(u) != null) {
-                for (int v : mp.get(u)) {
-                    indegree[v]--;
-                    if (indegree[v] == 0) {
-                        q.add(v);
-                        count++;
+        while (!st.isEmpty())
 
-                        result.add(v);
-                    }
-                }
-            }
+        {
+
+            result.add(st.pop());
+
         }
 
-        if (count == numCourses) {
-            return result.stream().mapToInt(i -> i).toArray();
-        }
-        return new int[] {};
+        return result.stream().mapToInt(i -> i).toArray();
     }
+
 }
