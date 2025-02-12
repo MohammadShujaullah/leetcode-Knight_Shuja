e4f5aa25f2b20ac52b9a79ccd699d1daac5556c7
Mohammad Shujaullah <mdshujaullah789@gmail.com>
2025-02-12 19:26:18 +0530

    Time: 22 ms (82.43%), Space: 95.5 MB (88.7%) - LeetHub

--- 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph.java
+++ 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph.java
@@ -1,44 +1,73 @@
 class Solution {
-    private long DFS(Map<Integer, ArrayList<Integer>> mp, int u, boolean visited[]) {
-        visited[u] = true;
-        long size = 1;  // count itself
 
-        if (mp.get(u) != null) {
-            for (int v : mp.get(u)) {
-                if (!visited[v]) {
-                    size += DFS(mp, v, visited);
-                }
-            }
+    private int find(int i, int parents[]) {
+        if (i == parents[i]) {
+            return i;
+
         }
-        return size;
+        return parents[i] = find(parents[i], parents);
+    }
 
+    private void union(int x, int y, int parents[], int rank[]) {
+        // path compresion and rank
+        int p_x = find(x, parents);
+        int p_y = find(y, parents);
+
+        if (p_x == p_y) {
+            return;
+        }
+        if (rank[p_x] > rank[p_y]) {
+            parents[p_y] = p_x;
+        } else if (rank[p_x] < rank[p_y]) {
+            parents[p_x] = p_y;
+        } else {
+            parents[p_x] = p_y;
+            rank[p_y]++;
+        }
     }
 
     public long countPairs(int n, int[][] edges) {
-        Map<Integer, ArrayList<Integer>> mp = new HashMap<>();
 
-        // Build adjacency list (undirected graph)
+        int parents[] = new int[n];
+
+        int rank[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            parents[i] = i;
+
+        }
+        // step 1
+
+        // make the components
+
         for (int[] edge : edges) {
-            mp.putIfAbsent(edge[0], new ArrayList<>());
-            mp.putIfAbsent(edge[1], new ArrayList<>()); // this method is of map is important
 
-            mp.get(edge[0]).add(edge[1]);
-            mp.get(edge[1]).add(edge[0]); // Add both directions
-        }
-        long totalcount = n;
-        long result = 0;
-        boolean visited[] = new boolean[n];
-
-        for (int u = 0; u < n; u++) {
-
-            if (!visited[u]) {
-                long sizeOfComponent = DFS(mp, u, visited);
-
-                result += (sizeOfComponent) * (totalcount - sizeOfComponent);
-                totalcount -= sizeOfComponent;
-            }
+            union(edge[0], edge[1], parents, rank);
 
         }
+
+        // step2
+        // make the hashmap ,to store the count of components node in each components
+
+        HashMap<Integer, Integer> map = new HashMap<>();   // <parent,size>
+        for (int i = 0; i < n; i++) {
+            int papaji=find(i,parents);
+
+           map.put(papaji,map.getOrDefault(papaji,0)+1);
+
+             
+        }
+
+        // step3
+        long result=0;
+        long remainingnode=n;
+
+        for(int i:map.values()){
+           result+=i*(remainingnode-i);
+           remainingnode-=i;
+
+        }
+
         return result;
 
     }
