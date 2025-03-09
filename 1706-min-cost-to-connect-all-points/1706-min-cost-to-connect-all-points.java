d6dd149e61bbff69fd1ef6c1ddc888bb40c56a94
Md Shujaullah <mdshujaullah789@gmail.com>
2025-03-09 16:19:07 +0530

    Time: 905 ms (8.05%), Space: 85.4 MB (20.74%) - LeetHub

--- 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.java
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.java
@@ -1,58 +1,85 @@
-class Node {
-    int node, weight;
-
-    public Node(int node, int weight) {
-        this.node = node;
-        this.weight = weight;
-
-    }
-}
 
 class Solution {
+    private int[] parent;
+    private int[] rank;
+
+    private int find(int x) {
+        if (x == parent[x])
+            return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    private void union(int x, int y) {
+        int xParent = find(x);
+        int yParent = find(y);
+
+        if (xParent == yParent)
+            return;
+
+        if (rank[xParent] > rank[yParent]) {
+            parent[yParent] = xParent;
+        } else if (rank[xParent] < rank[yParent]) {
+            parent[xParent] = yParent;
+        } else {
+            parent[xParent] = yParent;
+            rank[yParent]++;
+        }
+    }
+     private int kruskal(List<int[]> edges, int n) {
+        int sum = 0;
+        int edgesConnected = 0;
+
+        for (int[] edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+
+            int parentU = find(u);
+            int parentV = find(v);
+
+            if (parentU != parentV) {
+                union(u, v);
+                sum += wt;
+                edgesConnected++;
+            }
+        }
+
+        // Check if we formed a spanning tree
+        if (edgesConnected != n - 1) {
+            System.out.println("It's not a MST");
+        }
+
+        return sum;
+    }
+
     public int minCostConnectPoints(int[][] points) {
-        HashMap<Integer, ArrayList<int[]>> mp = new HashMap<>();
+        List<int[]> edges = new ArrayList<>();
 
         int n = points.length;
+        parent = new int[n];
+        rank = new int[n];
 
-        /// making adjecency map
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 0;
+        }
+
+        /// making adjecency List
         for (int i = 0; i < n; i++) { // i am taking indeces of points[][] as node
-            mp.put(i, new ArrayList<>());
+
             for (int j = 0; j < n; j++) {
                 if (i != j) {
                     int dist = Math.abs(points[i][0] - points[j][0]) + Math.abs(points[i][1] - points[j][1]);
-                    mp.get(i).add(new int[] { j, dist });
+                    edges.add(new int[] { i, j, dist });
 
                 }
             }
         }
-        boolean isMST[] = new boolean[n];
-        int totalcost = 0;
 
-        PriorityQueue<Node> pq = new PriorityQueue<>(Comparator.comparingInt(l -> l.weight));
-        pq.add(new Node(0, 0)); // initially distance is zero from zero node
+        // step1 , now we have to sort the list the
 
-        while (!pq.isEmpty()) {
-            Node pair = pq.poll();
+        edges.sort(Comparator.comparingInt(a -> a[2]));
+        return kruskal(edges,n);
 
-            int node = pair.node;
-            int dist = pair.weight;
-
-            if (isMST[node]) {
-                continue;
-            }
-            isMST[node] = true;
-            totalcost += dist;
-
-            for (int m[] : mp.get(node)) {
-                int nextnode = m[0];
-                int nextdist = m[1];
-
-                if (!isMST[nextnode]) {
-                    pq.add(new Node(nextnode, nextdist));
-                }
-            }
-        }
-
-        return totalcost;
     }
 }
