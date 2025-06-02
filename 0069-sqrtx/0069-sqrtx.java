89b6cb6343ae3e77033837e3b88fbb383baab202
Md Shujaullah <mdshujaullah789@gmail.com>
2025-06-02 13:56:21 +0530

    Time: 1 ms (86.79%), Space: 40.8 MB (69.48%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int mySqrt(int x) {       /// space O(1) and time=O(n)
+        int left=1;
+        int right=x;
+        int ans=0;
+
+
+        while(left<=right){
+
+            int mid=left+(right-left)/2;
+
+            if(mid*mid==x){
+                return mid;
+            }
+
+            else if((long)mid*mid<(long)x){
+                left=mid+1;
+                ans=mid;
+
+            }
+            else{
+                right=mid-1;
+
+            }
+            
+        }
+
+        return Math.round(ans);
+    }
+}
