b4abd565eab7a001ab9cfb881a39bd5ab9b45a69
Md Shujaullah <mdshujaullah789@gmail.com>
2025-06-15 19:04:57 +0530

    Time: 31 ms (63.94%), Space: 61.4 MB (73.71%) - LeetHub

--- 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.java
+++ 2022-maximum-alternating-subsequence-sum/2022-maximum-alternating-subsequence-sum.java
@@ -1,33 +1,18 @@
 class Solution {
-    int n;
-    long t[][];
-
-    private long solve(int idx, int[] nums, boolean flag) {
-        if (idx >= n)
-            return 0;
-
-        int f = flag ? 1 : 0;
-
-        if (t[idx][f] != 0) {
-            return t[idx][f];
-        }
-        long skip = solve(idx + 1, nums, flag);// when you are skipping, then no addition of val 
-
-        int val = nums[idx];
-        if (flag == false) {
-            val = -val;
-        }
-
-        long take = solve(idx + 1, nums, !flag) + val;
-
-        return t[idx][f] = Math.max(take, skip);
-
-    }
-
     public long maxAlternatingSum(int[] nums) {
-        n = nums.length;
-        t = new long[n][2];
+        int n = nums.length;
+        long t[][] = new long[n+1][2]; // 0 for true and 1 for odd 
 
-        return solve(0, nums, true);// 0 means even index, and treu means '+'
+        for (int i = 1; i < n+1; i++) {
+    
+                t[i][0] = Math.max(t[i - 1][1] - nums[i-1], t[i - 1][0]); // if number add at even idx
+
+                t[i][1] = Math.max(t[i - 1][0] + nums[i-1], t[i - 1][1]);//  for number at odd idx
+            
+
+        }
+
+        return Math.max(t[n][0], t[n][1]);
+
     }
 }
