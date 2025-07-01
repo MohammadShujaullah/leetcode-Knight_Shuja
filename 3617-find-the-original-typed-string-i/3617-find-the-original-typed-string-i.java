e0a2803c0c2eecf0861a8fd8a2159701048cc488
Md Shujaullah <mdshujaullah789@gmail.com>
2025-07-01 14:48:16 +0530

    Time: 1 ms (87.19%), Space: 42.1 MB (61.16%) - LeetHub

--- 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.java
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.java
@@ -2,12 +2,12 @@ class Solution {
     public int possibleStringCount(String word) {
         int count = 1;
 
-        HashMap<Character, Integer> mp = new HashMap<>();
-        for (int i = 0; i < word.length(); i++) {
-            char ch = word.charAt(i);
-            mp.put(ch, mp.getOrDefault(ch, 0) + 1);
+        // HashMap<Character, Integer> mp = new HashMap<>();
+        // for (int i = 0; i < word.length(); i++) {
+        //     char ch = word.charAt(i);
+        //     mp.put(ch, mp.getOrDefault(ch, 0) + 1);
 
-        }
+        // }
 
         for (int i = 1; i < word.length(); i++) {
 
