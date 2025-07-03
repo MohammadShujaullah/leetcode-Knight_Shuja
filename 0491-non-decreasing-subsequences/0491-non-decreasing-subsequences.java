fd1f75e49a10a4b1318ed8f31261b0266f0cb1f7
Md Shujaullah <mdshujaullah789@gmail.com>
2025-07-04 02:11:18 +0530

    Time: 8 ms (64%), Space: 51.4 MB (48.6%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.java
@@ -1,20 +1,22 @@
 class Solution {
     int n;
 
-    private void backTrack(int[] nums, int idx, List<Integer> curr, List<List<Integer>> result) {
+    private void backTrack(int nums[], int idx, List<Integer> curr, List<List<Integer>> result) {
         if (curr.size() >= 2) {
-            result.add(new ArrayList<>(curr)); // clone the list
-        }
+            result.add(new ArrayList<>(curr));
 
-        Set<Integer> set = new HashSet<>(); // prevent duplicates at this recursion level
+        }
+        Set<Integer> set = new HashSet<>();
 
         for (int i = idx; i < n; i++) {
             if ((curr.isEmpty() || nums[i] >= curr.get(curr.size() - 1)) && !set.contains(nums[i])) {
+
                 set.add(nums[i]); // mark number as used in this layer
 
                 curr.add(nums[i]);
                 backTrack(nums, i + 1, curr, result);
                 curr.remove(curr.size() - 1);
+
             }
         }
     }
@@ -24,6 +26,8 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> curr = new ArrayList<>();
         backTrack(nums, 0, curr, result);
+
         return result;
+
     }
 }
