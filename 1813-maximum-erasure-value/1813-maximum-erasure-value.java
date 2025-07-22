a9c743f31a82e7a750071255cb0d86144e820056
Md Shujaullah <mdshujaullah789@gmail.com>
2025-07-22 20:31:41 +0530

    Time: 55 ms (33.19%), Space: 57.9 MB (81.12%) - LeetHub

--- 1813-maximum-erasure-value/1813-maximum-erasure-value.java
+++ 1813-maximum-erasure-value/1813-maximum-erasure-value.java
@@ -1,44 +1,62 @@
 class Solution {
     public int maximumUniqueSubarray(int[] nums) {
-        // Arrays.sort(nums);
-        // int curr = nums[0];
-        // if (nums.length == 1)
-        //     return nums[0];
-        // int max = Integer.MIN_VALUE;
-        // for (int i = 1; i < nums.length; i++) {
-        //     if (nums[i] != nums[i - 1]) {
-        //         curr += nums[i];
+      
+
+        // int windowstart=0;
+        // int windowend=0;                         // varible length sliding window apply here 
+        // int maxsum=Integer.MIN_VALUE;
+        // HashMap<Integer,Integer>mp=new HashMap<>();
+
+        // while(windowend<nums.length){
+        //     int n=nums[windowend]; 
+        //     int currsum=0;
+
+        //     if(mp.containsKey(n) && mp.get(n)>=windowstart){
+        //         windowstart=mp.get(n)+1;
         //     }
 
-        //     max = Math.max(max, curr);
+        //     mp.put(n,windowend);
+        //    for(int i=windowstart;i<=windowend;i++){
+        //     currsum+=nums[i];
+        //    }
+
+
+        //     maxsum=Math.max(maxsum,currsum);
+        //     windowend++;
         // }
 
-        // return max;
+        //  return maxsum==Integer.MIN_VALUE?0:maxsum;
 
-        int windowstart=0;
-        int windowend=0;                         // varible length sliding window apply here 
-        int maxsum=Integer.MIN_VALUE;
-        HashMap<Integer,Integer>mp=new HashMap<>();
+         //---------------------above solution beats 5.30%----729ms---O(n^2)----------------------//
 
-        while(windowend<nums.length){
-            int n=nums[windowend];
-            int currsum=0;
 
-            if(mp.containsKey(n) && mp.get(n)>=windowstart){
-                windowstart=mp.get(n)+1;
+    HashSet<Integer>st=new HashSet<>();
+
+        int i=0;
+        int j=0;
+        int sum=0;
+        int result=0;
+        while(j<nums.length){
+            if(!st.contains(nums[j])){
+                sum+=nums[j];
+                result=Math.max(result,sum);
+                st.add(nums[j]);
+                j++;
+
             }
+            else{ /// we have to shrink the window now , untill when duplicate is seen
+                while(i<nums.length && st.contains(nums[j])){
+                    sum-=nums[i];
+                    st.remove(nums[i]);
+                    i++;
 
-            mp.put(n,windowend);
-           for(int i=windowstart;i<=windowend;i++){
-            currsum+=nums[i];
-           }
-
-
-            maxsum=Math.max(maxsum,currsum);
-            windowend++;
+                }
+            }
         }
 
-         return maxsum==Integer.MIN_VALUE?0:maxsum;
+        return result;
+
+
       
     }
 }
