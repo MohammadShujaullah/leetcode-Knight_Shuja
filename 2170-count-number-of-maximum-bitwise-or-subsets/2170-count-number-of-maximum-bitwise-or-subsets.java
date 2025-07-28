f1f1dc4179819bbd87b84de24667bf265564504a
Md Shujaullah <mdshujaullah789@gmail.com>
2025-07-29 00:46:24 +0530

    Time: 48 ms (27.93%), Space: 66.6 MB (5.54%) - LeetHub

--- 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.java
+++ 2170-count-number-of-maximum-bitwise-or-subsets/2170-count-number-of-maximum-bitwise-or-subsets.java
@@ -1,31 +1,40 @@
 class Solution {
     int n;
+    int t[][];
+
     private int countSubsets(int idx, int[] nums, int curror, int maxor) {
         if (idx == n) {
             if (curror == maxor) {
-                return 1; /// mughe ek subsequence mila jo , satisfy kr rjha h
-
+                return 1;
             }
-
             return 0;
+        }
+
+        if(t[idx][curror]!=-1){
+            return t[idx][curror];
+
         }
 
         int takecount = countSubsets(idx + 1, nums, curror | nums[idx], maxor);
         int skipcount = countSubsets(idx + 1, nums, curror, maxor);
 
-        return takecount + skipcount;
-
+        return t[idx][curror]=takecount + skipcount;
     }
 
     public int countMaxOrSubsets(int[] nums) {
-        n=nums.length;
+        n = nums.length;
         int maxor = 0;
-        for (int i = 0; i < nums.length; i++) {
-            maxor = maxor | nums[i];
+        for (int n : nums) {
+            maxor = maxor | n;
         }
+        t = new int[n + 1][maxor + 1];
+
+
+        for (int row[] : t) {
+            Arrays.fill(row, -1);
+        }
+
         int curror = 0;
-
         return countSubsets(0, nums, curror, maxor);
-
     }
 }
