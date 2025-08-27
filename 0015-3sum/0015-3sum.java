cb585f7be8575f4223ea51ae165d892ac9f95a75
Md Shujaullah <mdshujaullah789@gmail.com>
2025-08-28 01:56:27 +0530

    Time: 2368 ms (5%), Space: 53.8 MB (6.06%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -34,28 +34,28 @@ class Solution {
 
     /// another solution 
 
-    // public List<List<Integer>> threeSum(int[] nums) {
-    //     Set<List<Integer>> result = new HashSet<>();
-    //     Arrays.sort(nums); // sorting helps with duplicates
+    public List<List<Integer>> threeSum(int[] nums) {
+        Set<List<Integer>> result = new HashSet<>();
+        Arrays.sort(nums); // sorting helps with duplicates
 
-    //     for (int i = 0; i < nums.length - 2; i++) {
-    //         int target = -nums[i];
-    //         HashSet<Integer> seen = new HashSet<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            int target = -nums[i];
+            HashSet<Integer> seen = new HashSet<>();
 
-    //         for (int j = i + 1; j < nums.length; j++) {
-    //             int complement = target - nums[j];
-    //             if (seen.contains(complement)) {
-    //                 List<Integer> triplet = Arrays.asList(nums[i], nums[j], complement);
-    //                 Collections.sort(triplet);
-    //                 result.add(triplet);
-    //             }
-    //             seen.add(nums[j]);
-    //         }
-    //     }
+            for (int j = i + 1; j < nums.length; j++) {
+                int complement = target - nums[j];
+                if (seen.contains(complement)) {
+                    List<Integer> triplet = Arrays.asList(nums[i], nums[j], complement);
+                    Collections.sort(triplet);
+                    result.add(triplet);
+                }
+                seen.add(nums[j]);
+            }
+        }
 
-    //     return new ArrayList<>(result);
+        return new ArrayList<>(result);
 
-    //}
+    }
 
     // my understandable solution 
     //------- main concept of the question to solve is the -------/\-////\\\\\\\\\\\
@@ -63,56 +63,52 @@ class Solution {
     // 2.  avoid the duplicate element
     // 3. avoid the duplicate target element becuase wwe find the triplet of it befor so why find it again 
 
-  
+    //    private void twoSum(int[] nums, int k, List<List<Integer>> result, int target) {
+    //         int i = k, j = nums.length - 1;
 
-   private void twoSum(int[] nums, int k, List<List<Integer>> result, int target) {
-        int i = k, j = nums.length - 1;
+    //         while (i < j) {
+    //             if (nums[i] + nums[j] > target) {
+    //                 j--;
+    //             } else if (nums[i] + nums[j] < target) {
+    //                 i++;
+    //             } else {
+    //                 result.add(Arrays.asList(-target, nums[i], nums[j]));
 
-        while (i < j) {
-            if (nums[i] + nums[j] > target) {
-                j--;
-            } else if (nums[i] + nums[j] < target) {
-                i++;
-            } else {
-                result.add(Arrays.asList(-target, nums[i], nums[j]));
+    //                 while (i < j && nums[i] == nums[i + 1]) {
+    //                     i++;
+    //                 }
+    //                 while (i < j && nums[j] == nums[j - 1]) {
+    //                     j--;
+    //                 }
 
-                while (i < j && nums[i] == nums[i + 1]) {
-                    i++;
-                }
-                while (i < j && nums[j] == nums[j - 1]) {
-                    j--;
-                }
+    //                 i++;
+    //                 j--;
+    //             }
+    //         }
+    //     }
 
-                i++;
-                j--;
-            }
-        }
-    }
+    //     public List<List<Integer>> threeSum(int[] nums) {
+    //           List<List<Integer>> result=new ArrayList<>();
+    //         int n = nums.length;
 
-    public List<List<Integer>> threeSum(int[] nums) {
-          List<List<Integer>> result=new ArrayList<>();
-        int n = nums.length;
+    //         if (n < 3)
+    //             return new ArrayList<>();
 
-        if (n < 3)
-            return new ArrayList<>();
+    //         Arrays.sort(nums);
 
-        Arrays.sort(nums);
+    //         // fixed one element 
 
-        // fixed one element 
+    //         // i go till n-3
 
-        // i go till n-3
+    //         for (int i = 0; i <= n - 3; i++) {
+    //             if (i > 0 && nums[i] == nums[i - 1]) {
+    //                 continue;
+    //             }
 
-        for (int i = 0; i <= n - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
+    //           twoSum(nums, i + 1, result, -nums[i]); // it will find n2,n3 {n1,n2,n3}
 
-         
-          twoSum(nums, i + 1, result, -nums[i]); // it will find n2,n3 {n1,n2,n3}
+    //         }
 
-        }
-
-
-        return result;
-    }
+    //         return result;
+    //     }
 }
