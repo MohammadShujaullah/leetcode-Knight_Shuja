a5c3f7569f08053aebb5f6554755fbe8209b02ed
Md Shujaullah <mdshujaullah789@gmail.com>
2025-09-02 16:52:12 +0530

    Time: 0 ms (100%), Space: 57.5 MB (37.67%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,29 +1,55 @@
 class Solution {
-    public void rotate(int[] nums, int k) {
-        int n = nums.length;
-        k=k%n;
+    //public void rotate(int[] nums, int k) {
+    // int n = nums.length;
+    // k=k%n;              /// important step when rotation is greater than the size
 
-        int arr[] = new int[k];
-        int j = 0;
-     
-           for (int i = n - k; i < n; i++) {
-            arr[j] = nums[i];
-            j++;
-           }
-       
+    // int arr[] = new int[k];
+    // int j = 0;
+
+    //    for (int i = n - k; i < n; i++) {
+    //     arr[j] = nums[i];
+    //     j++;
+    //    }
+
+    // int m=n-k;
+    // int p=n-1;
+
+    // while(m-1>=0){
+    //    nums[p]=nums[m-1];
+    //    m--;
+    //    p--;
+
+    // }
+    // for(int i=0;i<arr.length;i++){
+    //      nums[i]=arr[i];
+    // }
+    //}
+
+    //------------Brute force ----with extra space -------O(n)----------------
+
+    private void  reverse(int nums[], int k , int i, int j) {
+
+        // first sections reverse
+
+        while (i <= j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+            i++;
+            j--;
+        }
         
-        int m=n-k;
-        int p=n-1;
-
-        while(m-1>=0){
-           nums[p]=nums[m-1];
-           m--;
-           p--;
-           
-        }
-        for(int i=0;i<arr.length;i++){
-             nums[i]=arr[i];
-        }
 
     }
+
+    public void rotate(int[] nums, int k) {
+         int n = nums.length;
+        k = k % n; /// important step when rotation is greater than the size
+       
+        reverse(nums, k, 0, n - k - 1);
+        reverse(nums, k, n - k, n - 1);
+        reverse(nums, k, 0, n - 1);
+
+    }
+
 }
