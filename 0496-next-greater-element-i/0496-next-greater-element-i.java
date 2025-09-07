575cf28a41ba93343b00f2f784c9fa42c22e3fa3
Md Shujaullah <mdshujaullah789@gmail.com>
2025-09-07 13:22:24 +0530

    Time: 4 ms (34.92%), Space: 44.4 MB (26.26%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,6 +1,48 @@
 class Solution {
+
+    // public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+    //     PriorityQueue<Integer> pq = new PriorityQueue<>();
+    //     int n = nums2.length;
+
+    //     Map<Integer,Integer> mp = new HashMap<>();
+
+    //     int ans[] = new int[nums1.length];
+
+    //     for (int i = n - 1; i >= 0; i--) {
+    //         if (pq.size() == 0) {
+    //             mp.put(nums2[i], -1);
+
+    //             pq.add(nums2[i]);
+    //             continue;
+    //         }
+    //         if (nums2[i] > pq.peek()) {
+    //             while (pq.size() > 0 && nums2[i] > pq.peek()) {
+    //                 pq.poll();
+    //             }
+
+    //         }
+
+    //         mp.put(nums2[i], (pq.size() != 0 ? pq.peek() : -1));
+    //         pq.add(nums2[i]);
+    //     }
+
+    //     for (int i = 0; i < nums1.length; i++) {
+    //         if (mp.containsKey(nums1[i])) {
+    //             ans[i] = mp.get(nums1[i]);
+    //         }
+    //     }
+
+    //     return ans;
+    // }
+
+    /////////////////--------above solution is uses PriorityQ+hashMap--------//////
+    ////////////----------but best solution is using Monotonic stack +HashMap------///
+
+    //// map ke andr search krna is very easy, O(1), so we use map
+
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+      Stack<Integer>st=new Stack<>();
+
         int n = nums2.length;
 
         Map<Integer,Integer> mp = new HashMap<>();
@@ -8,21 +50,24 @@ class Solution {
         int ans[] = new int[nums1.length];
 
         for (int i = n - 1; i >= 0; i--) {
-            if (pq.size() == 0) {
-                mp.put(nums2[i], -1);
-
-                pq.add(nums2[i]);
-                continue;
-            }
-            if (nums2[i] > pq.peek()) {
-                while (pq.size() > 0 && nums2[i] > pq.peek()) {
-                    pq.poll();
+           
+           
+                while (st.size() > 0 && nums2[i] > st.peek()) {
+                    st.pop();
                 }
 
+            
+             if (st.size() == 0) {
+                mp.put(nums2[i], -1);
+
+              
+            }
+            else{
+            mp.put(nums2[i], (st.size() != 0 ? st.peek() : -1));
+
             }
 
-            mp.put(nums2[i], (pq.size() != 0 ? pq.peek() : -1));
-            pq.add(nums2[i]);
+            st.push(nums2[i]);
         }
 
         for (int i = 0; i < nums1.length; i++) {
@@ -33,4 +78,5 @@ class Solution {
 
         return ans;
     }
+
 }
