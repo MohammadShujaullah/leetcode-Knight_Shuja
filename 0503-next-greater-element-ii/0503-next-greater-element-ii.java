b81c9996cf827b3f3d60f67de45be96cc440d214
Md Shujaullah <mdshujaullah789@gmail.com>
2025-09-07 17:35:45 +0530

    Time: 15 ms (69.55%), Space: 46.4 MB (38.4%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.java
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -4,26 +4,53 @@ class Solution {
     of the original arr and just copy the original arr twice in the newarr, so we can do the 
     circular arr traversal in single loop, no back pointer, EASY! this question can be easily 
     solved without stack, by the above-mentioned approach! */
-    
+
+    //-----------------Brute force O(n2)--------------------- by just traversing on nums double time ----//
+    // public int[] nextGreaterElements(int[] nums) {
+    //     int n=nums.length;
+    //     int ans[]=new int [n];
+    //     Arrays.fill(ans,Integer.MIN_VALUE);
+
+    //     for(int i=0;i<n;i++){
+    //         for(int j=i+1;j<=i+n-1;j++){
+    //             if(nums[i]<nums[j%n]){
+    //                 ans[i]=nums[j%n];
+    //                 break;
+    //             }
+    //         }
+    //         if(ans[i]==Integer.MIN_VALUE){
+    //             ans[i]=-1;
+    //         }
+    //     }
+
+    //     return ans;
+
+    // }
+
+    /////////////--------------------O(4n)=O(2n*2)  , as pushing and poping element takes 2 times   
     public int[] nextGreaterElements(int[] nums) {
-        int n=nums.length;
-        int ans[]=new int [n];
-        Arrays.fill(ans,Integer.MIN_VALUE);
+        int n = nums.length;
 
+        int ans[] = new int[n];
+        Stack<Integer> st = new Stack<>();
 
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<=i+n-1;j++){
-                if(nums[i]<nums[j%n]){
-                    ans[i]=nums[j%n];
-                    break;
-                }
+        for (int i = 2 * n - 1; i >= 0; i--) { // as you double the array hypothetically, then the last inx is tarted from 2*n-1
+            while (!st.isEmpty() && nums[i % n] >= st.peek()) {
+                st.pop();
+
             }
-            if(ans[i]==Integer.MIN_VALUE){
-                ans[i]=-1;
+
+            // but just after i<n, you have to store the next greater in ans[]
+
+            if (i < n) {
+                ans[i] = (st.size() > 0 ? st.peek() : -1);
+
             }
+            st.push(nums[i%n]);
+
         }
-
         return ans;
 
     }
+
 }
