a90a04833394e0292e8fedc503d0b142b6fb2b3e
Md Shujaullah <mdshujaullah789@gmail.com>
2025-09-12 16:17:48 +0530

    Time: 18 ms (17.59%), Space: 45.9 MB (16.8%) - LeetHub

---
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
@@ -0,0 +1,51 @@
+class Solution {
+
+    private boolean isVowel(char ch) {
+        char m = Character.toLowerCase(ch);
+        if (m == 'a' || m == 'e' || m == 'i' || m == 'o' || m == 'u') {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean doesAliceWin(String s) {
+
+        int count = 0;
+
+        for (int i=0;i<s.length();i++) {
+            if (isVowel(s.charAt(i))) {
+                count++;
+            }
+        }
+
+        if (count == 0)
+            return false;
+
+        if (count % 2 == 1)
+            return true;
+        
+        // boolean oddtrue=true;
+        // int vowelcount=0;
+        // for(int i=0;i<s.length();i++){
+        //      if(oddtrue && isVowel(s.charAt(i))){
+
+        //         vowelcount++;
+        //         if(vowelcount=count-1 || vowelcount%2==1){
+        //             oddtrue=false;
+        //         }
+
+        //      }
+        //      else if(!oddtrue && !isVowel(s.charAt(i))){
+        //         voowelcount=0;
+        //         oddtrue=true;
+        //      }
+        // }
+        // return false;    
+
+        if(count%2==0)return true;
+
+
+        return false;
+
+    }
+}
