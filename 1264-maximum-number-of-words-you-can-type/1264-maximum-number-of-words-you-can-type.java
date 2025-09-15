2f791968296591abbaa6ed9ff8ac14f172a34882
Md Shujaullah <mdshujaullah789@gmail.com>
2025-09-16 00:52:23 +0530

    Time: 4 ms (27.38%), Space: 41.9 MB (93.74%) - LeetHub

--- 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.java
+++ 1264-maximum-number-of-words-you-can-type/1264-maximum-number-of-words-you-can-type.java
@@ -1,31 +1,63 @@
 class Solution {
+    // public int canBeTypedWords(String text, String brokenLetters) {
+    //     int count=0;
+    //     HashSet<Character>set=new HashSet<>();
+
+    //     for(int i=0;i<brokenLetters.length();i++){
+    //         set.add(brokenLetters.charAt(i));
+    //     }
+
+    //     String words[]=text.split(" ");
+
+    //     for(String word:words){
+    //          count++;
+    //         for(int i=0;i<word.length();i++){
+    //             if(set.contains(word.charAt(i))){
+    //                 count--;
+    //                 break;
+    //             }
+    //         }
+
+    //     }
+
+    //     return count;
+    //}
+    //-------------------------/// above code , beats 92%----------------------------------
+
     public int canBeTypedWords(String text, String brokenLetters) {
-        int count=0;
-        HashSet<Character>set=new HashSet<>();
 
-        for(int i=0;i<brokenLetters.length();i++){
-            set.add(brokenLetters.charAt(i));
+        // we use 26 sized arraY 
+
+        boolean mp[] = new boolean[26];
+
+        for (int i = 0; i < brokenLetters.length(); i++) {
+            mp[brokenLetters.charAt(i) - 'a'] = true;
         }
 
-        String words[]=text.split(" ");
+        boolean notbrk = true;
+        int result = 0;
 
+        for (int i = 0; i < text.length(); i++) {
 
+            if (text.charAt(i) == ' ') {
 
+                if (notbrk == true) {
+                    result++;
 
-        for(String word:words){
-             count++;
-            for(int i=0;i<word.length();i++){
-                if(set.contains(word.charAt(i))){
-                    count--;
-                    break;
                 }
+                notbrk = true; // reset for another word 
+            } else if (mp[text.charAt(i) - 'a'] == true) {
+                notbrk = false;
+
             }
-           
 
         }
 
-        return count;
+        if (notbrk) {
+            result++;
+        }
 
-        
+        return result;
     }
+
 }
