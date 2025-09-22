f2d75d30658f9ca205c0d587655530928f7ad623
Md Shujaullah <mdshujaullah789@gmail.com>
2025-09-23 03:18:14 +0530

    Time: 1 ms (99.44%), Space: 42 MB (93.72%) - LeetHub

--- 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.java
+++ 3242-count-elements-with-maximum-frequency/3242-count-elements-with-maximum-frequency.java
@@ -1,33 +1,67 @@
 class Solution {
-    public int maxFrequencyElements(int[] nums) {
-        HashMap<Integer,Integer>mp=new HashMap<>();
+    // public int maxFrequencyElements(int[] nums) {
+    //     HashMap<Integer,Integer>mp=new HashMap<>();
 
-        for(int i=0;i<nums.length;i++){
-            mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
+    //     for(int i=0;i<nums.length;i++){
+    //         mp.put(nums[i],mp.getOrDefault(nums[i],0)+1);
 
-        }
-        int max=0;
+    //     }
+    //     int max=0;
 
-        for(Map.Entry<Integer,Integer>entry:mp.entrySet()){
-            int key=entry.getKey();
-            int value=entry.getValue();
+    //     for(Map.Entry<Integer,Integer>entry:mp.entrySet()){
+    //         int key=entry.getKey();
+    //         int value=entry.getValue();
 
-            if(value>max){
-                max=value;
+    //         if(value>max){
+    //             max=value;
 
+    //         }
+    //     }
+    //     int count=0;
+
+    //     for(int x:mp.keySet()){
+    //         if(mp.get(x)==max){
+    //             count+=max;
+    //         }
+    //     }
+
+    //     return count;
+
+
+
+    
+
+
+
+
+
+    // }
+    ///////O(2n) time complexity
+
+
+
+
+// for O(n) time
+     public int maxFrequencyElements(int[] nums){
+        int freq[]=new int[101];
+
+        int maxfreq=0;
+        int total=0;
+
+
+        for(int num:nums){
+            freq[num]++;
+
+            if(maxfreq<freq[num]){
+                maxfreq=freq[num];
+                total=maxfreq;
             }
-        }
-        int count=0;
-
-        for(int x:mp.keySet()){
-            if(mp.get(x)==max){
-                count+=max;
+            else if(maxfreq==freq[num]){
+                total+=maxfreq;
             }
+
         }
 
-        return count;
-
-
-
-    }
+        return total;
+     }
 }
