bf0ec700d0af250625494190df04aebe8e16d4e0
Md Shujaullah <mdshujaullah789@gmail.com>
2025-09-28 19:56:31 +0530

    Time: 0 ms (100%), Space: 45.6 MB (22.08%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,21 +1,44 @@
 class Solution {
-    // missing number, method 1, using (sum of n term), used for not much big n 
-    public int missingNumber(int[] nums) {
 
+    // // missing number, method 1, using (sum of n term), used for not much big n 
+    // public int missingNumber(int[] nums) {
+
+    //     int n=nums.length;
+    //      int expectedsum=n*(n+1)/2;
+
+    //      int actualsum=0;
+
+    //      for(int i=0;i<nums.length;i++){
+    //         actualsum+=nums[i];
+
+    //      }
+
+
+    //      return (expectedsum-actualsum);
+    // }
+
+
+
+    //using XOR property , that XOR^0=XOR
+     public int missingNumber(int[] nums){
         int n=nums.length;
-         int expectedsum=n*(n+1)/2;
 
-         int actualsum=0;
-
-         for(int i=0;i<nums.length;i++){
-            actualsum+=nums[i];
-
-         }
+        int xor=0;
+        for(int i=0;i<=nums.length;i++){
+            xor^=i;
+        }
 
 
-         return (expectedsum-actualsum);
-         
+        for(int i=0;i<nums.length;i++){
+            xor^=nums[i];
+
+        }
 
 
-    }
+        return xor;
+
+
+     }
+
+
 }
