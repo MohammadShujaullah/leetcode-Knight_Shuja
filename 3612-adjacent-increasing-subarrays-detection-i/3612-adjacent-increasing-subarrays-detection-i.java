73f349496abcb222f160e7986457cb62fd5f6a2e
Md Shujaullah <mdshujaullah789@gmail.com>
2025-10-28 01:40:56 +0530

    Time: 1 ms (100%), Space: 44.3 MB (90.84%) - LeetHub

--- 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.java
+++ 3612-adjacent-increasing-subarrays-detection-i/3612-adjacent-increasing-subarrays-detection-i.java
@@ -3,8 +3,8 @@ class Solution {
         int n = nums.size();
 
         for (int start = 0; start + 2 * k <= n; start++) {
-            boolean firstInc = isIncreasing(nums, start, start + k);
-            boolean secondInc = isIncreasing(nums, start + k, start + 2 * k);
+            boolean firstInc = isIncreasing(nums, start, start + k-1);
+            boolean secondInc = isIncreasing(nums, start + k, start + 2 * k-1);
             if (firstInc && secondInc)
                 return true;
         }
@@ -12,7 +12,7 @@ class Solution {
     }
 
     private boolean isIncreasing(List<Integer> nums, int start, int end) {
-        for (int i = start + 1; i < end; i++) {
+        for (int i = start + 1; i <= end; i++) {
             if (nums.get(i) <= nums.get(i - 1))
                 return false;
         }
