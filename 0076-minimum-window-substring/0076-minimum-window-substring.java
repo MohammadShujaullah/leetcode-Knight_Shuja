61847bd26fbfa038284de2dc2929fe9c3d96ab75
Md Shujaullah <mdshujaullah789@gmail.com>
2025-11-07 02:35:07 +0530

    Time: 15 ms (69.06%), Space: 47 MB (6.17%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,48 +1,55 @@
 class Solution {
     public String minWindow(String s, String t) {
+        HashMap<Character, Integer> freqmap = new HashMap<>();
+        // [populate the map with t string ]
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            freqmap.put(ch, freqmap.getOrDefault(ch, 0) + 1);
+
+        }
+        int uniquecharcount = freqmap.size();
+        int startidx = -1;
+        int windowstart = 0;
+        int windowend = 0;
+        int minlen = Integer.MAX_VALUE;
         int n = s.length();
-        int m = t.length();
-
-        Map<Character, Integer> mp = new HashMap<>();
-
-        for (char c : t.toCharArray()) {
-            mp.put(c, mp.getOrDefault(c, 0) + 1);
-
-        }
-
-        int countreq = m;
-        int minwindow = Integer.MAX_VALUE;
-        int start = 0;
-
-        int i = 0;
-
-        for (int j = 0; j < n; j++) {
-
-            if (mp.containsKey(s.charAt(j)) && mp.get(s.charAt(j)) > 0) {
-
-                countreq--;
-
+        while (windowend < n) {
+            char ch = s.charAt(windowend);
+            if (freqmap.containsKey(ch)) {
+                // expanding phase
+                freqmap.put(ch, freqmap.get(ch) - 1);
+                if (freqmap.get(ch) == 0) {
+                    uniquecharcount--;
+                }
             }
-            mp.put(s.charAt(j), mp.getOrDefault(s.charAt(j), 0) - 1);
-
-            //shrinking 
-
-            while (countreq == 0) {
-                if (minwindow > (j - i + 1)) {
-                    start = i;
-                    minwindow = j - i + 1;
+            // shrinking phase
+            while (uniquecharcount == 0) {
+                // finding length
+                int len = windowend - windowstart + 1;
+                if (len < minlen) {
+                    minlen = len;
+                    startidx = windowstart;
 
                 }
-                mp.put(s.charAt(i), mp.getOrDefault(s.charAt(i), 0) + 1);
-
-                if (mp.getOrDefault(s.charAt(i), 0) > 0) {
-                    countreq++;
+                ch = s.charAt(windowstart);
+                if (freqmap.containsKey(ch)) {
+                    freqmap.put(ch, freqmap.get(ch) + 1);
+                    if (freqmap.get(ch) > 0) {
+                        uniquecharcount++;
+                    }
                 }
-                i++;
+                windowstart++;
 
             }
-        }
 
-        return minwindow == Integer.MAX_VALUE? "" : s.substring(start, minwindow + start);
+            windowend++;
+
+        }
+        if (startidx == -1) {
+            return "";
+
+        }
+        return s.substring(startidx, startidx + minlen);
+
     }
 }
