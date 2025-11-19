5a35edd25f4ed6deff4b42bc25a338e8c83f9ce0
Md Shujaullah <mdshujaullah789@gmail.com>
2025-11-19 17:27:32 +0530

    Time: 0 ms (100%), Space: 42.8 MB (9.95%) - LeetHub

--- 1252-break-a-palindrome/1252-break-a-palindrome.java
+++ 1252-break-a-palindrome/1252-break-a-palindrome.java
@@ -1,37 +1,32 @@
 class Solution {
-    private boolean isPalindrome(StringBuilder sb){
-        StringBuilder ss=new StringBuilder(sb);
-        String rev=ss.reverse().toString();
+    private boolean isPalindrome(StringBuilder sb) {
+        StringBuilder ss = new StringBuilder(sb);
+        String rev = ss.reverse().toString();
 
         return (rev.equals(sb.toString()));
     }
+
     public String breakPalindrome(String palindrome) {
-        StringBuilder sb=new StringBuilder(palindrome);
+        StringBuilder sb = new StringBuilder(palindrome);
 
-        for(int i=0;i<sb.length();i++){
-            if(sb.charAt(i)-'a'>0 && sb.charAt(i)-'a'<=25  ){
+        for (int i = 0; i < sb.length() / 2; i++) {
+            if (sb.charAt(i) != 'a') {
 
-                char ch=sb.charAt(i);
+                sb.setCharAt(i, 'a');
 
-                sb.setCharAt(i,'a');
-
-                if(!isPalindrome(sb)){
+                if (!isPalindrome(sb)) {
                     return sb.toString();
-                }
-                else{
-                    sb.setCharAt(i,ch);
                 }
 
             }
         }
 
-        if(sb.length()>1){
-            sb.setCharAt(sb.length()-1,'b');
+        if (sb.length() > 1) {
+            sb.setCharAt(sb.length() - 1, 'b');
             return sb.toString();
-            
+
         }
 
-        
         return "";
 
     }
