0cac91f08397b05f766dba85add7248d6af129e8
Md Shujaullah <mdshujaullah789@gmail.com>
2025-12-23 23:43:30 +0530

    Time: 0 ms (100%), Space: 42.9 MB (25.03%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,50 +1,28 @@
 class Solution {
     // without memoization ,the code giving me TLE,so use Memo
+    int t[];
 
+    private int solve(int[] nums, int i, int n) {
+        if (i >= nums.length) {
+            return 0;
+        }
 
-    // int t[];
+        if (t[i] != -1)
+            return t[i];
 
-    // private int solve(int[] nums, int i, int n) {     // top down approch.
-    //     if (i >= nums.length) {
-    //         return 0;
-    //     }
+        int steal = nums[i] + solve(nums, i + 2, n);
+        int skip = solve(nums, i + 1, n);
 
-    //     if (t[i] != -1)
-    //         return t[i];
+        return t[i] = Math.max(steal, skip);
 
-    //     int steal = nums[i] + solve(nums, i + 2, n);
-    //     int skip = solve(nums, i + 1, n);
-
-    //     return t[i] = Math.max(steal, skip);
-
-    // }
+    }
 
     public int rob(int[] nums) {
+      int n=nums.length;                         // memo is always generally same 
 
+      t=new int [n+1];
+      Arrays.fill(t,-1);
 
-        // Bottom up approch 
-       int n= nums.length;
-       if(n==1){
-        return nums[0];
-
-       }
-
-       int t[]=new int [n+1];
-       // t[i]  is   max. stolen money till i house 
-
-       // no house 
-       t[0]=0;
-
-       // 1 house 
-       t[1]=nums[0];
-       for(int i=2;i<=n;i++){
-        int steal=nums[i-1]+t[i-2];
-        int skip=t[i-1];
-
-        t[i]=Math.max(steal,skip);
-
-       }
-       return t[n];
- 
+      return solve(nums,0,n);
     }
 }
