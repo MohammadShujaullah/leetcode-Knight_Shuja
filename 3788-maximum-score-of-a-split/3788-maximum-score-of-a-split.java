e1b92b169ead529096dd088789a34534f860279b
Md Shujaullah <mdshujaullah789@gmail.com>
2025-12-29 02:13:52 +0530

    Time: 3 ms (100%), Space: 122.6 MB (0%) - LeetHub

--- 3788-maximum-score-of-a-split/3788-maximum-score-of-a-split.java
+++ 3788-maximum-score-of-a-split/3788-maximum-score-of-a-split.java
@@ -1,29 +1,53 @@
 class Solution {
+    // public long maximumScore(int[] nums) {
+    //     int n=nums.length;
+
+    //     int suff[]=new int[n];
+    //      suff[n-1]=nums[n-1];
+
+    //     for(int i=n-2;i>=0;i--){
+    //         suff[i]=Math.min(nums[i],suff[i+1]);
+
+    //     }
+
+    //     long  prefsum=0;
+
+    //     long maxscore=Integer.MIN_VALUE;
+
+    //     for(int i=0;i<n-1;i++){
+    //         prefsum+=nums[i];
+
+    //         long score=prefsum-suff[i+1];
+    //         maxscore=Math.max(maxscore,score);
+
+    //     }
+
+    //     return maxscore;
+    // }
+
     public long maximumScore(int[] nums) {
-        int n=nums.length;
+        int n = nums.length;
 
-        int suff[]=new int[n];
-         suff[n-1]=nums[n-1];
+        int suff[] = new int[n];
 
+        suff[n - 1] = nums[n - 1];
 
-        for(int i=n-2;i>=0;i--){
-            suff[i]=Math.min(nums[i],suff[i+1]);
-            
+        for (int i = n - 2; i >= 0; i--) {
+            suff[i] = Math.min(nums[i], suff[i + 1]);
+
         }
 
+        long max = Integer.MIN_VALUE;
+        long pref = 0;
+        for (int i = 0; i < n - 1; i++) {
+            pref += nums[i];
 
-        long  prefsum=0;
+            long score = pref - suff[i + 1];
 
-        long maxscore=Integer.MIN_VALUE;
+            max = Math.max(max, score);
 
-        for(int i=0;i<n-1;i++){
-            prefsum+=nums[i];
-
-            long score=prefsum-suff[i+1];
-            maxscore=Math.max(maxscore,score);
-            
         }
 
-        return maxscore;
+        return max;
     }
 }
