75333136b0af3a7d3e10fd66f16d8167e1781bf7
Md Shujaullah <mdshujaullah789@gmail.com>
2025-12-29 22:04:11 +0530

    Time: 1 ms (99.96%), Space: 42.7 MB (100%) - LeetHub

--- 3789-minimum-cost-to-acquire-required-items/3789-minimum-cost-to-acquire-required-items.java
+++ 3789-minimum-cost-to-acquire-required-items/3789-minimum-cost-to-acquire-required-items.java
@@ -1,60 +1,30 @@
 class Solution {
-    // public long minimumCost(int cost1, int cost2, int costBoth, int need1, int need2) {
-    //     if (need1 == 0 && need2 == 0)
-    //         return 0;
-
-    //     long priceForBoth = Math.min(costBoth, (long) cost1 + cost2);
-
-    //     long priceForOnly1 = Math.min(cost1, costBoth);
-    //     long priceForOnly2 = Math.min(cost2, costBoth);
-
-    //     long totalcost = 0;
-
-    //     long common = Math.min(need1, need2);
-
-    //     totalcost += common * priceForBoth;
-
-    //     if (need1 > need2) {
-    //         totalcost += (long) (need1 - common) * priceForOnly1;
-
-    //     } else if (need2 > need1) {
-    //         totalcost += (long) (need2 - common) * priceForOnly2;
-
-    //     }
-
-    //     return totalcost;
-
-    // }
     public long minimumCost(int cost1, int cost2, int costBoth, int need1, int need2) {
-        if (need1 == 0 && need2 == 0) {
+        if (need1 == 0 && need2 == 0)
             return 0;
 
+        long priceForBoth = Math.min(costBoth, (long) cost1 + cost2);
+
+        long priceForOnly1 = Math.min(cost1, costBoth);
+        long priceForOnly2 = Math.min(cost2, costBoth);
+
+        long totalcost = 0;
+
+        long common = Math.min(need1, need2);
+
+        totalcost += common * priceForBoth;
+
+        if (need1 > need2) {
+            totalcost += (long) (need1 - common) * priceForOnly1;
+
+        } else if (need2 > need1) {
+            totalcost += (long) (need2 - common) * priceForOnly2;
+
         }
-        long mincostboth=Math.min(costBoth,(long)cost1+cost2);
 
-        long mincost1=Math.min(costBoth,cost1);
-        long mincost2=Math.min(costBoth,cost2);
-
-
-        long common=Math.min(need1,need2);
-
-         long totalcost=0;
-
-
-         totalcost+=common*mincostboth;
-
-
-         if(need1>need2){
-            totalcost+=(need1-need2)*mincost1;
-
-
-         }
-         else{
-            totalcost+=(need2-need1)*mincost2;
-         }
-
-
-         return totalcost;
+        return totalcost;
 
     }
+    
+    
 }
