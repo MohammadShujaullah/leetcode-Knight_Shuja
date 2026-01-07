d48eba7b30f89fa9261c00e161c1e525598c0b8d
Md Shujaullah <mdshujaullah789@gmail.com>
2026-01-07 23:46:24 +0530

    Time: 6 ms (74.32%), Space: 60.2 MB (44.75%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.java
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.java
@@ -17,34 +17,48 @@
 class Solution {
 
     long Tsum = 0;
-    long maxP = 0;   
+     long maxP = 0;
 
+    // how to find the total sum of the Binart TREE
     private long TotalSum(TreeNode root) {
-        if (root == null) return 0;
+        if (root == null) {
+            return 0;
+        }
 
-        return root.val + TotalSum(root.left) + TotalSum(root.right);
+        long sum = root.val + TotalSum(root.left) + TotalSum(root.right);
+
+        return sum;
+
     }
 
     private long subtreeSum(TreeNode root) {
-        if (root == null) return 0;
 
-        long subtree1 = root.val 
-                + subtreeSum(root.left) 
-                + subtreeSum(root.right);
-
+        if (root == null) {
+            return 0;
+        }
+        long subtree1 = root.val + subtreeSum(root.left) + subtreeSum(root.right);
         long subtree2 = Tsum - subtree1;
 
-        maxP = Math.max(maxP, subtree1 * subtree2);  
+        maxP = Math.max(maxP, subtree2 * subtree1);
 
         return subtree1;
+
     }
 
     public int maxProduct(TreeNode root) {
+
         int MOD = 1_000_000_007;
 
-        Tsum = TotalSum(root);
-        subtreeSum(root);
+        if (root == null) {
+            return 0;
 
+        }
+       
+        Tsum = TotalSum(root);
+
+       
+        subtreeSum(root);
         return (int)(maxP % MOD);
+
     }
 }
