f208cf66d4aba7bd832463cf4096c08c8d40bce5
Md Shujaullah <mdshujaullah789@gmail.com>
2026-01-19 00:30:45 +0530

    Time: 3 ms (100%), Space: 219.2 MB (0%) - LeetHub

--- 3809-best-reachable-tower/3809-best-reachable-tower.java
+++ 3809-best-reachable-tower/3809-best-reachable-tower.java
@@ -1,42 +1,82 @@
 class Solution {
+    // public int[] bestTower(int[][] towers, int[] center, int radius) {
+    //     int cx = center[0];
+    //     int cy = center[1];
+
+    //     int maxQuality = -1;
+    //     int bestX = 0;
+    //     int bestY = 0;
+
+    //     for (int[] tower : towers) {
+    //         int x = tower[0];
+    //         int y = tower[1];
+    //         int quality = tower[2];
+
+    //         long manhattenDistance = Math.abs((long) x - cx) + Math.abs((long) y - cy);
+
+    //         if (manhattenDistance <= radius) {
+
+    //             if (maxQuality == -1) {
+    //                 maxQuality = quality;
+    //                 bestX = x;
+    //                 bestY = y;
+    //             }
+
+    //             if (quality > maxQuality || (quality == maxQuality && (x < bestX || (x == bestX && y < bestY)))) {
+    //                 maxQuality = quality;
+    //                 bestX = x;
+    //                 bestY = y;
+
+    //             }
+
+    //         }
+
+    //     }
+
+    //     if (maxQuality == -1) {
+    //         return new int[] { -1, -1 };
+    //     }
+
+    //     return new int[] { bestX, bestY };
+    // }
+
     public int[] bestTower(int[][] towers, int[] center, int radius) {
         int cx = center[0];
         int cy = center[1];
 
-        int maxQuality = -1;
-        int bestX = 0;
-        int bestY = 0;
+        int maxq = -1;
+        int bestx = 0;
+        int besty = 0;
 
-        for (int[] tower : towers) {
-            int x = tower[0];
-            int y = tower[1];
-            int quality = tower[2];
+        for (int t[] : towers) {
+            int x = t[0];
+            int y = t[1];
+            int q = t[2];
 
-            long manhattenDistance = Math.abs((long) x - cx) + Math.abs((long) y - cy);
+            int dist = Math.abs(cx - x) + Math.abs(cy - y);
 
-            if (manhattenDistance <= radius) {
+            if (dist <= radius) {
+                //reachable
 
-                if (maxQuality == -1) {
-                    maxQuality = quality;
-                    bestX = x;
-                    bestY = y;
+                if (q > maxq) {
+                    maxq = q;
+                    bestx = x;
+                    besty = y;
+
+                } else if (q == maxq) {
+                    if (bestx > x || (bestx == x && besty > y)) {
+                        bestx = x;
+                        besty = y;
+                    }
                 }
-
-                if (quality > maxQuality || (quality == maxQuality && (x < bestX || (x == bestX && y < bestY)))) {
-                    maxQuality = quality;
-                    bestX = x;
-                    bestY = y;
-
-                }
-
             }
+        }
+
+        if(maxq==-1){
+            return new int[]{-1,-1};
 
         }
 
-        if (maxQuality == -1) {
-            return new int[] { -1, -1 };
-        }
-
-        return new int[] { bestX, bestY };
+        return new int[]{bestx,besty};
     }
 }
