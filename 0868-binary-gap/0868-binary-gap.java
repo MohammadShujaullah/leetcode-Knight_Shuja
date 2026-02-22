2356eda840d2138639460deca2a1bc0c5c73fc63
Md Shujaullah <mdshujaullah789@gmail.com>
2026-02-23 02:02:02 +0530

    Time: 1 ms (40.73%), Space: 42.4 MB (48.38%) - LeetHub

--- 0868-binary-gap/0868-binary-gap.java
+++ 0868-binary-gap/0868-binary-gap.java
@@ -1,5 +1,7 @@
 class Solution {
     public int binaryGap(int n) {
+
+        //simple and brute force , and easy to do , without the Bitwise 
         String Binary=Integer.toBinaryString(n);
 
         int max=0;
@@ -18,4 +20,24 @@ class Solution {
         return max;
 
     }
+
+
+  //100 % accepted
+    //  public int binaryGap(int n) {
+    //     int max=0;
+    //     int prev=-1;
+    //     int next=0;
+    //     while(n>0){
+    //         if((n&1)==1){
+    //             if(prev!=-1){
+    //                 max=Math.max(max,next-prev);
+                    
+    //             }
+    //             prev=next;
+    //         }
+    //           n >>= 1;
+    //           next++;
+    //     }
+    //     return max;
+    // }
 }
