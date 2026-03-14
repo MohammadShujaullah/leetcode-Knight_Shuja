24c105a2c057d380dd2ce9e95fef4bfc7356cfad
Md Shujaullah <mdshujaullah789@gmail.com>
2026-03-15 01:32:59 +0530

    Time: 11 ms (44.23%), Space: 46.7 MB (42.59%) - LeetHub

---
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -0,0 +1,39 @@
+class Solution {
+
+    private void solve(int n,StringBuilder curr,ArrayList<String>result){
+        if(curr.length()==n){
+            result.add(curr.toString());
+            return;
+        }
+
+        for(char ch='a';ch<='c';ch++){
+            if(curr.length()>0 && curr.charAt(curr.length()-1)==ch){
+                continue;
+            }
+
+            // do work
+            curr.append(ch);
+
+            //explore
+            solve(n,curr,result);
+
+            //undo
+
+            curr.deleteCharAt(curr.length()-1);
+
+        }
+    }
+    public String getHappyString(int n, int k) {
+        StringBuilder curr=new StringBuilder();
+        ArrayList<String>result=new ArrayList<>();
+
+        solve(n,curr,result);
+        if(result.size()<k){
+            return "";
+
+        }
+
+        return result.get(k-1);
+
+    }
+}
